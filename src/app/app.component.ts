2cfef3209badcd01ca8bcd826385694e493cff2d
daniere <dannyreuben5@gmail.com>
2021-12-03 07:59:44 +0200

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   `
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Managementbc';
+  pageTitle: string = 'Acme Product Managementb';
 }
